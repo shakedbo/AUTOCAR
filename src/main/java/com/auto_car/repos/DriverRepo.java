8e24fd44aead52bae9e9c4d5b9fc819227cb146e
shaked <shakedbozi@gmail.com>
2020-06-29 13:03:24 +0300

    Driver class

---
+++ src/main/java/com/auto_car/repos/DriverRepo.java
@@ -0,0 +1,4 @@
+package com.auto_car.repos;
+
+public class DriverRepo {
+}
---
+++ src/main/java/drivers/Driver.java
@@ -0,0 +1,14 @@
+package drivers;
+
+import java.util.Date;
+
+public class Driver
+{
+    private String firstName;
+    private String lastName;
+    private int id;
+    private int licenseNumber;
+    private String licenseType;
+    private Date licenseExpiredDate;
+
+}
