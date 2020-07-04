457464671757621121b442bfd408f288f2c89f21
shaked <shakedbozi@gmail.com>
2020-07-04 15:48:03 +0300

    drivers repo update

--- src/main/java/com/auto_car/repos/DriverRepo.java
+++ src/main/java/com/auto_car/repos/DriverRepo.java
@@ -1,4 +1,44 @@
 package com.auto_car.repos;
+import com.auto_car.model.Vehicle;
+import drivers.Driver;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class DriverRepo {
+
+    private final List<Driver> drivers = new ArrayList<>();
+
+    public List<Driver> getDrivers() {
+        return drivers;
+    }
+
+    public void addDriver(Driver driver) {
+        for (Driver current : this.drivers) {
+            if (driver.getId().equals(current.getId())) {
+                return;
+            }
+        }
+
+        this.drivers.add(driver);
+    }
+
+    public void removeVehicle(String driverIdToRemove) {
+        for (Driver current : this.drivers) {
+            if (driverIdToRemove.equals(current.getId())) {
+                this.drivers.remove(current);
+                break;
+            }
+        }
+    }
+
+    public Driver getDriverById(String driverId) {
+        for (Driver driver : this.drivers) {
+            if (driver.getId().equals(driverId)) {
+                return driver;
+            }
+        }
+        return null;
+
+    }
 }
--- src/main/java/drivers/Driver.java
+++ src/main/java/drivers/Driver.java
@@ -4,11 +4,94 @@ import java.util.Date;
 
 public class Driver
 {
+
     private String firstName;
     private String lastName;
-    private int id;
+    private String id;
     private int licenseNumber;
     private String licenseType;
     private Date licenseExpiredDate;
+    private int drivingExperience;
+    private int dayliMileage;
 
+
+    public void SetDayliMileage (){};
+
+    public void GetdDayliMileage(){return ;};
+
+    public void SetDayliFuel (){};
+
+    public void SetParkingReports(){};
+
+    public void SetDrivingReporrts(){};
+
+    public void GetDayliFuel (){};
+
+    public void GetParkingReports(){};
+
+    public void GetDrivingReporrts(){};
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(int licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getLicenseType() {
+        return licenseType;
+    }
+
+    public void setLicenseType(String licenseType) {
+        this.licenseType = licenseType;
+    }
+
+    public Date getLicenseExpiredDate() {
+        return licenseExpiredDate;
+    }
+
+    public void setLicenseExpiredDate(Date licenseExpiredDate) {
+        this.licenseExpiredDate = licenseExpiredDate;
+    }
+
+    public int getDrivingExperience() {
+        return drivingExperience;
+    }
+
+    public void setDrivingExperience(int drivingExperience) {
+        drivingExperience = drivingExperience;
+    }
+
+    public int getDayliMileage() {
+        return dayliMileage;
+    }
+
+    public void setDayliMileage(int dayliMileage) {
+        this.dayliMileage = dayliMileage;
+    }
 }
