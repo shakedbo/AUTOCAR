ec70f17f9053d9eaf2eb338d728e55cb561b8466
shaked <shakedbozi@gmail.com>
2020-07-07 00:16:25 +0300

    Add classes

--- src/main/java/com/auto_car/expenses/CarUse.java
+++ src/main/java/com/auto_car/expenses/CarUse.java
@@ -1,8 +1,15 @@
 package com.auto_car.expenses;
 
 public class CarUse implements Expense {
+    private String licensePlate;
+
     @Override
     public double calculatePrice() {
         return 0;
+    }
+
+    @Override
+    public String getLicensePlate() {
+        return this.licensePlate;
     }
 }
--- src/main/java/com/auto_car/expenses/GarageVisit.java
+++ src/main/java/com/auto_car/expenses/GarageVisit.java
@@ -9,4 +9,9 @@ public class GarageVisit implements Expense {
     public double calculatePrice() {
         return price;
     }
+
+    @Override
+    public String getLicensePlate() {
+        return licensePlate;
+    }
 }
