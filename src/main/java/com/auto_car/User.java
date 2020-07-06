7ac8c95225b805738695b13098acec00014ff590
annul <annul1992@gmail.com>
2020-07-06 22:08:56 +0300

    new

---
+++ src/main/java/com/auto_car/User.java
@@ -0,0 +1,76 @@
+package com.auto_car;
+
+import com.auto_car.model.Vehicle;
+
+public class User
+{
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFistName() {
+        return fistName;
+    }
+
+    public void setFistName(String fistName) {
+        this.fistName = fistName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private  int id ;
+    private  String fistName;
+    private  String lastName;
+    private  int phoneNumber;
+    private  String email;
+    private  String password;
+
+public void addVehicle()
+{
+
+
+
+}
+
+public void addVehicle(Vehicle car)
+{
+
+
+
+}
