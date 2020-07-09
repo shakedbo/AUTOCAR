a92afcc5c07eb681ca8780a32dddc6f3608fe890
shaked <shakedbozi@gmail.com>
2020-07-09 10:55:35 +0300

    Add classes

--- src/main/java/com/auto_car/repos/VehicleRepo.java
+++ src/main/java/com/auto_car/repos/VehicleRepo.java
@@ -1,16 +1,30 @@
 package com.auto_car.repos;
 
+import com.auto_car.expenses.Ticket;
+import com.auto_car.model.PrivateCar;
 import com.auto_car.model.Vehicle;
+import com.auto_car.model.VehicleExpenses;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class VehicleRepo {
 
-    private VehicleRepo(){}
+    private VehicleRepo(){
+        Vehicle vehicle = new PrivateCar();
+        vehicle.setLicensePlate("125-48-354");
+
+        VehicleExpenses vehicleExpenses = new VehicleExpenses(vehicle.getLicensePlate());
+        vehicleExpenses.addExpense(new Ticket(new Date(2020,1,1),100));
+        vehicleExpenses.addExpense(new Ticket(new Date(2020,1,2),2000));
+        vehicle.setVehicleExpenses(vehicleExpenses);
+
+        addVehicle(vehicle);
+    }
     private volatile static VehicleRepo instance;
     private static final Object lock = new Object();
-
     public static VehicleRepo getInstance(){
         if(instance == null){
             synchronized (lock){
